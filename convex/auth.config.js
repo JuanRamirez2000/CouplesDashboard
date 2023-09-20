000f80f19e1223a44257324aae902fe77d100535
Juan Ramirez <juanpram2000@gmail.com>
2023-09-20 13:01:40 -0700

    changed up some enviornment variables

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -2,7 +2,7 @@
 export default {
   providers: [
     {
-      domain: "https://evident-fox-14.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ]
